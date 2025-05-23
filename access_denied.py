4caecdbadf7bb7fafb0082bf910a70361181f7e5
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 13:52:40 +0300

    Create access_denied.py
    # Access Denied Simulation
    
    This simple Python script simulates a system login response.
    
    ## What it does:
    - Prompts the user for their name.
    - Greets the user.
    - Displays a message indicating that access is denied.
    - Provides further instructions to contact the system administrator.

---
+++ access_denied.py
@@ -0,0 +1,13 @@
+start = input('Waiting for result')  # Originally: "Ожидание результата"
+print('')
+
+user_name = input('Enter your username: ')  # Originally: "Введите имя пользователя: "
+greeting = 'Hello '
+print(greeting + user_name)
+
+intro = "Unfortunately, you do not have access to the system."
+info = "Please contact the system administrator."
+
+print(intro)
+print(info)
+print('')
