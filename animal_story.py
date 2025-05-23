ff8461931852b7a0f3a531ffd42c71ee118cec1a
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 14:00:27 +0300

    Update animal_story.py
    # This script prompts the user to enter two animal names,
    # representing the classic fable "The Tortoise and the Hare".
    # It then prints a simple sentence describing their actions:
    # the hare is sleeping, while the tortoise is walking.

--- animal_story.py
+++ animal_story.py
@@ -1,8 +1,3 @@
-# This script prompts the user to enter two animal names,
-# representing the classic fable "The Tortoise and the Hare".
-# It then prints a simple sentence describing their actions:
-# the hare is sleeping, while the tortoise is walking.
-
 first_animal = input('"Hare"')
 second_animal = input('"Tortoise"')
 print('"Hare sleeps, Tortoise walks"')
