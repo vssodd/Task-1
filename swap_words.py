c9e533ebe1ffe9bdd3ab0e29af19462fa6ad665a
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 13:56:16 +0300

    Create swap_words.py
    The code takes two inputs from the user.
    
    Prints the inputs in the order entered.
    
    Swaps the values of variables a and b.
    
    Prints them again, but swapped.

---
+++ swap_words.py
@@ -0,0 +1,8 @@
+a = input('Enter the first word: ')
+b = input('Enter the second word: ')
+
+print(a, b)    # Print words in original order
+
+a, b = b, a    # Swap the two words
+
+print(a, b)    # Print words after swapping
